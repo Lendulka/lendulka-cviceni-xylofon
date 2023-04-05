2ad919b7b27039f1bd24fd7cf6b28c594ad08a09
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-04-05 23:22:01 +0200

    Přidán JS

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 console.log('Funguju')
+
+const buttons = document.querySelectorAll('button')
+buttons.forEach((button) => {
+    button.addEventListener('click', (event) => played(event))
+})
+
+const played = (event) => {
+    let heading = document.querySelector('h1')
+    heading.innerHTML = event.target.innerHTML
+    let sound = new Audio(`tony/${event.target.innerHTML}.mp3`)
+    sound.play()
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('animationend', (event) => {
+        event.target.classList.remove('zatres')
+    })
+})
+
+document.addEventListener('keydown', (event) => {
+    if (event.code.startsWith('Digit') || event.code.startsWith('Numpad')) {
+        let keyIndex = (Number(event.code.slice(-1)) - 1)
+        if (keyIndex >= 0 && keyIndex < buttons.length) {
+            let button = buttons[keyIndex]
+            button.click()
+            button.classList.add('zatres')
+        }
+    }
+})
+
+
+
